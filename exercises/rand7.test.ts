159476590109312a8a0458625c9b86d2f89dc279
Damian Senn <senn@puzzle.ch>
2019-01-22 21:54:53 +0100

    Add rand7 which minimized calls to rand5

--- exercises/rand7.test.ts
+++ exercises/rand7.test.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai'
 import { range } from '../utils/range'
-import { rand7 } from './rand7'
+import { rand7, rand7m5 } from './rand7'
 
 describe('rand7', () => {
   it('should return number between 1 and 7', () => {
@@ -8,6 +8,25 @@ describe('rand7', () => {
 
     for (let i of range(7000)) {
       let r = rand7()
+
+      m.set(r, (m.get(r) || 0) + 1)
+    }
+
+    expect(m.size).to.equal(7)
+
+    for (let v of m.values()) {
+      expect(v).to.be.above(900)
+      expect(v).to.be.below(1100)
+    }
+  })
+})
+
+describe('rand7m5', () => {
+  it('should return number between 1 and 7', () => {
+    let m = new Map()
+
+    for (let i of range(7000)) {
+      let r = rand7m5()
 
       m.set(r, (m.get(r) || 0) + 1)
     }
--- exercises/rand7.ts
+++ exercises/rand7.ts
@@ -11,3 +11,12 @@ export function rand7(): number {
   }
   return (r % 7) + 1
 }
+
+export function rand7m5(): number {
+  let rnd = '1234567'.repeat(3)
+  let r = 0
+  do {
+    r = Number(rnd[(rand5() - 1) * 5 + (rand5() - 1)]) | 0
+  } while (r === 0)
+  return r
+}
