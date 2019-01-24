7a9d45916bd96a3a516f17161d83140d5e446720
Damian Senn <senn@puzzle.ch>
2019-01-24 16:50:25 +0100

    E 3

---
+++ project-euler/0003.ts
@@ -0,0 +1,11 @@
+import { primes } from '../utils/prime'
+
+let n = 600851475143
+let factors = []
+for (let prime of primes(10000)) {
+  if (n % prime === 0) {
+    n /= prime
+    factors.push(prime)
+  }
+}
+console.log(factors[factors.length - 1])
--- utils/prime.ts
+++ utils/prime.ts
@@ -6,6 +6,7 @@ const primeReducer = (primes: Set<number>, isPrime: boolean, prime: number) =>
 
 export function primes(max: number): Set<number> {
   let primes = Array.from(range(max), returnTrue)
+  primes[0] = primes[1] = false
 
   for (let i of range(2, Math.sqrt(max))) {
     if (primes[i]) {
