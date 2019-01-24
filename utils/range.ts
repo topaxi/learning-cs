689fed55c72e432b702fee7456e37fd577399745
Damian Senn <senn@puzzle.ch>
2019-01-24 19:21:52 +0100

    Add project callback to range util

--- utils/range.ts
+++ utils/range.ts
@@ -1,12 +1,12 @@
-export function* range(
+export function* range<T = number>(
   start: number,
   end?: number,
-  { step = 1 } = {}
-): IterableIterator<number> {
+  { step = 1, project = (i: number): T => i as any } = {}
+): IterableIterator<T> {
   if (end === undefined) {
     end = start
     start = 0
   }
 
-  for (let i = start; i < end; i += step) yield i
+  for (let i = start; i < end; i += step) yield project(i)
 }
