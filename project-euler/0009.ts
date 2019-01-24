9a74a54210626404532d0d5e993eb52ba5775b2a
Damian Senn <senn@puzzle.ch>
2019-01-24 21:08:18 +0100

    E 9

---
+++ project-euler/0009.ts
@@ -0,0 +1,12 @@
+import { range } from '../utils/range'
+
+for (let a of range(500)) {
+  for (let b of range(500)) {
+    let c = Math.sqrt(a ** 2 + b ** 2)
+
+    if (a + b + c === 1000) {
+      console.log(a * b * c)
+      process.exit()
+    }
+  }
+}
