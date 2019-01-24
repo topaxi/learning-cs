89d76e837149324e5384781ecb88b86a932c9f0f
Damian Senn <senn@puzzle.ch>
2019-01-24 15:27:40 +0100

    Missed edge case in staircase

--- algorithms/dynamic-programming/staircase.ts
+++ algorithms/dynamic-programming/staircase.ts
@@ -2,6 +2,7 @@ import { HashMap } from '../../data-structures/hash/hash-map'
 import { Y, memoizedY } from '../../utils/y'
 
 const staircase = (staircase: (n: number) => number) => (n: number) => {
+  if (n < 1) return 0
   if (n < 2) return 1
   if (n === 2) return 2
 
