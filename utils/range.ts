f288833af26968bd157464645f548f02f8f71055
Damian Senn <senn@puzzle.ch>
2019-01-24 21:03:44 +0100

    Add inclusive flag to range helper

--- utils/range.ts
+++ utils/range.ts
@@ -1,11 +1,15 @@
 export function* range<T = number>(
   start: number,
   end?: number,
-  { step = 1, project = (i: number): T => i as any } = {}
+  { step = 1, inclusive = false, project = (i: number): T => i as any } = {}
 ): IterableIterator<T> {
   if (end === undefined) {
     end = start
     start = 0
+  }
+
+  if (inclusive) {
+    end++
   }
 
   for (let i = start; i < end; i += step) yield project(i)
