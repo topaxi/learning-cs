3b9bdb144a7aa57a40181136210bf16e80b9947c
Damian Senn <senn@puzzle.ch>
2019-02-12 14:15:21 +0100

    Add WeakSingleParamStore for memoize fn

--- utils/array.ts
+++ utils/array.ts
@@ -1,8 +1,8 @@
-import { memoize, SingleParamStore } from './memo'
+import { memoize, WeakSingleParamStore } from './memo'
 
 export const tail = memoize<(<T>(list: ReadonlyArray<T>) => ReadonlyArray<T>)>(
   list => list.slice(1),
-  new SingleParamStore()
+  new WeakSingleParamStore()
 )
 
 export const head = <T>(list: ReadonlyArray<T>): T => list[0]
--- utils/memo.ts
+++ utils/memo.ts
@@ -1,6 +1,24 @@
 import { MemoTrie } from '../data-structures/tree/memo-trie'
+import { ClearableWeakmap } from './clearable-weakmap'
 
 export class SingleParamStore<T extends any[], U> extends Map<T, U> {
+  get(key: T) {
+    return super.get(key[0])
+  }
+
+  set(key: T, value: U) {
+    return super.set(key[0], value)
+  }
+
+  has(key: T) {
+    return super.has(key[0])
+  }
+}
+
+export class WeakSingleParamStore<T extends any[], U> extends ClearableWeakmap<
+  T,
+  U
+> {
   get(key: T) {
     return super.get(key[0])
   }
