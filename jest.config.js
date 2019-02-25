27702856c0b4137577e561a222697091947d44b4
Damian Senn <senn@puzzle.ch>
2019-02-25 10:44:31 +0100

    Collect coverage for untested files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-};
+  collectCoverageFrom: [
+    'algorithms/**/*.ts',
+    'data-structures/**/*.ts',
+    'exercises/**/*.ts',
+    'utils/**/*.ts'
+  ]
+}
