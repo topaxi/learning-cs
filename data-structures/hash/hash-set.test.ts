22c80c0a4f37251ccec6f048e2bcb8dbeadb2463
Damian Senn <senn@puzzle.ch>
2019-02-26 14:35:00 +0100

    HashSet#union()

--- data-structures/hash/hash-set.test.ts
+++ data-structures/hash/hash-set.test.ts
@@ -23,4 +23,44 @@ describe('HashSet<T>', () => {
     expect(set.has(1)).toBe(false)
     expect(set.has(2)).toBe(false)
   })
+
+  test('should be iterable over all the values', () => {
+    let set = new HashSet()
+
+    set.add(1)
+    set.add(2)
+    set.add(3)
+
+    expect([...set]).toEqual([1, 2, 3])
+  })
+
+  describe('#union()', () => {
+    test('should create an union of the given iterables', () => {
+      let set = new HashSet()
+
+      set.add(1)
+
+      expect(set.union()).not.toBe(set)
+
+      set = set.union(new HashSet().add(2).add(3))
+
+      expect(set.toArray()).toEqual([1, 2, 3])
+
+      set = set.union(new HashSet().add(4), [5], [6, 7])
+
+      expect(set.toArray()).toEqual([1, 2, 3, 4, 5, 6, 7])
+    })
+  })
+
+  describe('#toArray()', () => {
+    test('should convert to an array', () => {
+      let set = new HashSet()
+
+      set.add(1)
+      set.add(2)
+      set.add(3)
+
+      expect(set.toArray()).toEqual([1, 2, 3])
+    })
+  })
 })
--- data-structures/hash/hash-set.ts
+++ data-structures/hash/hash-set.ts
@@ -1,19 +1,43 @@
 import { HashMap } from './hash-map'
 
 export class HashSet<T extends string | number> {
-  private _hash = new HashMap<T, true>()
+  private _hash = new HashMap<T, T>()
 
   has(value: T): boolean {
     return this._hash.has(value)
   }
 
   add(value: T): this {
-    this._hash.set(value, true)
+    this._hash.set(value, value)
     return this
   }
 
   delete(value: T): this {
     this._hash.delete(value)
     return this
+  }
+
+  union(...iterables: Array<Iterable<T>>): HashSet<T> {
+    let set = new HashSet<T>()
+
+    iterables.unshift(this)
+
+    for (let i = 0; i < iterables.length; i++) {
+      for (let value of iterables[i]) {
+        set.add(value)
+      }
+    }
+
+    return set
+  }
+
+  *[Symbol.iterator](): IterableIterator<T> {
+    for (let value of this._hash.values()) {
+      yield value
+    }
+  }
+
+  toArray(): T[] {
+    return [...this]
   }
 }
