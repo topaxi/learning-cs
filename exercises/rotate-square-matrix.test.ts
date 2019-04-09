b725e7b5d75a710ce1ce33cf13f043308ca3df61
Damian Senn <senn@puzzle.ch>
2019-04-09 11:43:42 +0200

    Rotate square matrix

---
+++ exercises/rotate-square-matrix.test.ts
@@ -0,0 +1,46 @@
+import { rotateSquareMatrix } from './rotate-square-matrix'
+
+describe('rotateSquareMatrix()', () => {
+  test('should rotate square matrix', () => {
+    let matrix = [
+      [1, 1, 1, 1, 1],
+      [2, 2, 2, 2, 2],
+      [3, 3, 3, 3, 3],
+      [4, 4, 4, 4, 4],
+      [5, 5, 5, 5, 5]
+    ]
+
+    expect(rotateSquareMatrix(matrix)).toEqual([
+      [5, 4, 3, 2, 1],
+      [5, 4, 3, 2, 1],
+      [5, 4, 3, 2, 1],
+      [5, 4, 3, 2, 1],
+      [5, 4, 3, 2, 1]
+    ])
+
+    matrix = [[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3], [4, 4, 4, 4]]
+
+    expect(rotateSquareMatrix(matrix)).toEqual([
+      [4, 3, 2, 1],
+      [4, 3, 2, 1],
+      [4, 3, 2, 1],
+      [4, 3, 2, 1]
+    ])
+
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    expect(rotateSquareMatrix(matrix)).toEqual([
+      [7, 4, 1],
+      [8, 5, 2],
+      [9, 6, 3]
+    ])
+
+    matrix = [[1, 2], [3, 4]]
+
+    expect(rotateSquareMatrix(matrix)).toEqual([[3, 1], [4, 2]])
+
+    matrix = [[1]]
+
+    expect(rotateSquareMatrix(matrix)).toEqual([[1]])
+  })
+})
---
+++ exercises/rotate-square-matrix.ts
@@ -0,0 +1,21 @@
+import { range, swap } from '../utils'
+
+function swapDiagonal(obj: number[][], a: number, b: number): void {
+  ;[obj[b][a], obj[a][b]] = [obj[a][b], obj[b][a]]
+}
+
+export function rotateSquareMatrix<T extends number[][]>(matrix: T): T {
+  for (let row of range(matrix.length)) {
+    for (let column of range(row + 1, matrix.length)) {
+      swapDiagonal(matrix, row, column)
+    }
+  }
+
+  for (let row of range(matrix.length)) {
+    for (let column of range(matrix.length / 2)) {
+      swap(matrix[row], column, matrix.length - column - 1)
+    }
+  }
+
+  return matrix
+}
