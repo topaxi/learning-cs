e73d4f65b7e9aa9f2e7245cd299e2f91fcf9aab1
Damian Senn <senn@puzzle.ch>
2019-04-19 19:30:09 +0200

    Add tests for memoize shortcuts

--- utils/memoize/memoize.test.ts
+++ utils/memoize/memoize.test.ts
@@ -19,4 +19,26 @@ describe('memoize', () => {
     expect(fn(a, b, c)).toBe(fn(a, b, c))
     expect(fn(b, c, a)).not.toBe(fn(a, b, c))
   })
+
+  describe('.unary', () => {
+    test('should memoize unary function', () => {
+      let fn = memoize.unary((a: any) => [a])
+      let a = 'a'
+      let b = 'b'
+
+      expect(fn(a)).toBe(fn(a))
+      expect(fn(b)).not.toBe(fn(a))
+    })
+  })
+
+  describe('.weak', () => {
+    test('should memoize unary function', () => {
+      let fn = memoize.weak.unary((a: any) => [a])
+      let a = { a: 'a' }
+      let b = { b: 'b' }
+
+      expect(fn(a)).toBe(fn(a))
+      expect(fn(b)).not.toBe(fn(a))
+    })
+  })
 })
