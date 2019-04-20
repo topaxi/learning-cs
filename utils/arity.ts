d9ad78dddb44fd4a4fb4962b3b5315573af34d35
Damian Senn <senn@puzzle.ch>
2019-04-20 13:07:12 +0200

    Fix arity function typings

--- utils/arity.ts
+++ utils/arity.ts
@@ -1,26 +1,46 @@
 import { pa } from './partial'
 
+export type NullaryFunction<R> = () => R
+export type UnaryFunction<T, R> = (v0: T) => R
+export type BinaryFunction<T, U, R> = (v0: T, v1: U) => R
+export type TernaryFunction<T, U, V, R> = (v0: T, v1: U, v2: V) => R
+export type NAryFunction<T, R> = (...args: T[]) => R
+
+export type AsNullaryFunction = <R>(
+  fn: NullaryFunction<R>
+) => NullaryFunction<R>
+export type AsUnaryFunction = <T, R>(
+  fn: UnaryFunction<T, R>
+) => UnaryFunction<T, R>
+export type AsBinaryFunction = <T, U, R>(
+  fn: BinaryFunction<T, U, R>
+) => BinaryFunction<T, U, R>
+export type AsTernaryFunction = <T, U, V, R>(
+  fn: TernaryFunction<T, U, V, R>
+) => TernaryFunction<T, U, V, R>
+
 export function arity<T, U, V, R>(
   n: 3,
-  fn: (v0: T, v1: U, v3: V) => R
-): (v0: T, v1: U, v3: V) => R
+  fn: TernaryFunction<T, U, V, R>
+): TernaryFunction<T, U, V, R>
 export function arity<T, U, R>(
   n: 2,
-  fn: (v0: T, v1: U) => R
-): (v0: T, v1: U) => R
-export function arity<T, R>(n: 1, fn: (value: T) => R): (value: T) => R
-export function arity<R>(n: 0, fn: () => R): () => R
+  fn: BinaryFunction<T, U, R>
+): BinaryFunction<T, U, R>
+export function arity<T, R>(n: 1, fn: UnaryFunction<T, R>): UnaryFunction<T, R>
+export function arity<R>(n: 0, fn: NullaryFunction<R>): NullaryFunction<R>
 export function arity<T>(
   n: number,
-  fn: (...args: any[]) => T
-): (...args: unknown[]) => T
+  fn: NAryFunction<any, T>
+): NAryFunction<unknown, T>
 export function arity<T>(
   n: number,
-  fn: (...args: any[]) => T
-): (...args: unknown[]) => T {
+  fn: NAryFunction<any, T>
+): NAryFunction<unknown, T> {
   return (...args: unknown[]) => fn(...args.slice(0, n))
 }
 
-export const arity1 = pa(arity, 1)
-export const arity2 = pa(arity, 2)
-export const arity3 = pa(arity, 3)
+export const arity0 = pa(arity, 0) as AsNullaryFunction
+export const arity1 = pa(arity, 1) as AsUnaryFunction
+export const arity2 = pa(arity, 2) as AsBinaryFunction
+export const arity3 = pa(arity, 3) as AsTernaryFunction
