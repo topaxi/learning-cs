2538aefb361d664119df2238df3fdebc436fe2cf
Damian Senn <senn@puzzle.ch>
2019-04-21 16:43:07 +0200

    Use simple BinarySearchTree for k-smallest test
    AvlTree seems to have some issues

--- exercises/k-smallest.test.ts
+++ exercises/k-smallest.test.ts
@@ -1,12 +1,12 @@
 import { range, shuffleInplace } from '../utils'
-import { AvlTree } from '../data-structures'
+import { BinarySearchTree } from '../data-structures'
 import { kSmallestBinarySearchTree } from './k-smallest'
 
 describe('k-smallest', () => {
   describe('using binary tree', () => {
     test('should return k smallest value', () => {
       let values = shuffleInplace(Array.from(range(20)))
-      let tree = AvlTree.from(values)
+      let tree = BinarySearchTree.from(values)
 
       expect(kSmallestBinarySearchTree(tree, 0)).toBe(-1)
       expect(kSmallestBinarySearchTree(tree, 1)).toBe(0)
