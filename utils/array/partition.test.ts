05657dc301f4fd161fa778a8df4d48ab434f04ea
Damian Senn <senn@puzzle.ch>
2019-04-22 17:33:47 +0200

    Add partition test

--- utils/array/partition.test.ts
+++ utils/array/partition.test.ts
@@ -1,11 +1,24 @@
-import { partitionInline } from './partition'
+import { partition, partitionInline } from './partition'
 
-describe('partitionInline', () => {
-  test('should partition array inline', () => {
-    let array = [0, 1, 0, 1, 0, 1, 0, 1]
+describe('utils/array/partition', () => {
+  describe('partition()', () => {
+    test('should partition array', () => {
+      let array = [0, 1, 0, 1, 0, 1, 0, 1]
 
-    partitionInline(array, array.length / 2, n => n === 0)
+      expect(partition(array, n => n === 0)).toEqual([
+        [0, 0, 0, 0],
+        [1, 1, 1, 1]
+      ])
+    })
+  })
 
-    expect(array).toEqual([0, 0, 0, 0, 1, 1, 1, 1])
+  describe('partitionInline()', () => {
+    test('should partition array inline', () => {
+      let array = [0, 1, 0, 1, 0, 1, 0, 1]
+
+      partitionInline(array, array.length / 2, n => n === 0)
+
+      expect(array).toEqual([0, 0, 0, 0, 1, 1, 1, 1])
+    })
   })
 })
