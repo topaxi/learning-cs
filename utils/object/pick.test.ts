0d28b9fbd493b4ec9cbe990608c61eb0b13a21a3
Damian Senn <senn@puzzle.ch>
2019-04-23 17:44:23 +0200

    Add omit helper

--- utils/object/pick.test.ts
+++ utils/object/pick.test.ts
@@ -1,10 +1,21 @@
-import { pick } from './pick'
+import { pick, omit } from './pick'
 
-describe('utils/pick', () => {
-  test('it should pick properties from object', () => {
-    let obj = { a: 1, b: 2, c: 3 }
+describe('utils/object/pick', () => {
+  describe('pick()', () => {
+    test('it should pick properties from object', () => {
+      let obj = { a: 1, b: 3, c: 3 }
 
-    expect(pick('a')(obj)).toEqual({ a: 1 })
-    expect(pick('a', 'c')(obj)).toEqual({ a: 1, c: 3 })
+      expect(pick('a')(obj)).toEqual({ a: 1 })
+      expect(pick('a', 'c')(obj)).toEqual({ a: 1, c: 3 })
+    })
+  })
+
+  describe('omit()', () => {
+    test('it should omit properties from object', () => {
+      let obj = { a: 1, b: 2, c: 3 }
+
+      expect(omit('a')(obj)).toEqual({ b: 2, c: 3 })
+      expect(omit('a', 'c')(obj)).toEqual({ b: 2 })
+    })
   })
 })
--- utils/object/pick.ts
+++ utils/object/pick.ts
@@ -1,10 +1,18 @@
 import { includedIn } from '../filters'
-import { c, arity2, paR } from '../function'
+import { c, arity2, paR, not } from '../function'
 import { prop } from './prop'
 
 type ObjectIndex = string | number | symbol
 type Entry<T, K extends keyof T = keyof T> = [K, T[K]]
 type PickPredicate<T> = (entry: Entry<T>, index: number, obj: T) => boolean
+type Omit<T, P extends keyof T> = Pick<T, Exclude<keyof T, P>>
+type PickByFn = <T>(predicate: PickPredicate<T>) => (o: T) => Partial<T>
+type PickFn = <P extends ObjectIndex>(
+  ...keys: P[]
+) => <T extends Record<P, unknown>>(o: T) => Pick<T, P>
+type OmitFn = <P extends ObjectIndex>(
+  ...keys: P[]
+) => <T extends Record<ObjectIndex, unknown>>(o: T) => Omit<T, P>
 
 const entries: <T>(o: T) => Entry<T>[] = Object.entries
 const fromEntries = <T>(entries: Entry<T>[]): T =>
@@ -13,10 +21,14 @@ const fromEntries = <T>(entries: Entry<T>[]): T =>
     return o
   }, {}) as T
 
-export const pickBy = <T>(o: T, predicate: PickPredicate<T>): Partial<T> =>
+export const pickBy: PickByFn = predicate => o =>
   fromEntries(entries(o).filter(arity2(paR(predicate, o))))
 
-export function pick<P extends ObjectIndex>(...keys: P[]) {
-  return <T extends Record<P, unknown>>(o: T): Pick<T, P> =>
-    pickBy(o, c(includedIn<P, keyof T>(keys), prop<Entry<T>, 0>(0))) as any
-}
+export const omitBy: PickByFn = c(pickBy, not) as any
+
+const props = <R extends Record<ObjectIndex, unknown>>(
+  ...keys: (keyof R)[]
+): PickPredicate<R> => c(includedIn(keys), prop<Entry<R>, 0>(0))
+
+export const pick: PickFn = c(pickBy, props) as any
+export const omit: OmitFn = c(omitBy, props) as any
