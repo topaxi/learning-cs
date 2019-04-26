01d0b6f8275c88ecd61e6fdd84edf131aff3703c
Damian Senn <senn@puzzle.ch>
2019-04-26 11:54:07 +0200

    ClearbaleWeakmap coverage

--- utils/function/memoize/clearable-weakmap.test.ts
+++ utils/function/memoize/clearable-weakmap.test.ts
@@ -7,10 +7,35 @@ describe('utils/function/memoize/clearable-weakmap', () => {
 
     map.set(obj, true)
 
+    expect(map.has(obj)).toBe(true)
     expect(map.get(obj)).toBe(true)
 
     map.clear()
 
+    expect(map.has(obj)).toBe(false)
     expect(map.get(obj)).toBeUndefined()
+  })
+
+  test('should be able to delete values', () => {
+    let map = new ClearableWeakmap()
+    let obj = {}
+
+    map.set(obj, true)
+
+    expect(map.has(obj)).toBe(true)
+    expect(map.get(obj)).toBe(true)
+
+    map.delete(obj)
+
+    expect(map.has(obj)).toBe(false)
+    expect(map.get(obj)).toBeUndefined()
+  })
+
+  describe('[Symbol.toStringTag]()', () => {
+    test('should return constructor name', () => {
+      let map = new ClearableWeakmap()
+
+      expect(String(map)).toBe('[object ClearableWeakmap]')
+    })
   })
 })
