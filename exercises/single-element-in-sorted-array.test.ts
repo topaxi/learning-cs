023bc5c6d288a83eed63b68a0d6f2fa7f7d038d8
Damian Senn <senn@puzzle.ch>
2019-04-26 23:54:08 +0200

    Single Element in a Sorted Array

---
+++ exercises/single-element-in-sorted-array.test.ts
@@ -0,0 +1,10 @@
+import { singleNonDuplicate } from './single-element-in-sorted-array'
+
+describe('540. Single Element in a Sorted Array', () => {
+  test.each<[number[], number]>([
+    [[3, 3, 7, 7, 10, 11, 11], 10],
+    [[1, 1, 2, 3, 3, 4, 4, 8, 8], 2]
+  ])('singleNonDuplicate(%o) is %o', (numbers, expected) => {
+    expect(singleNonDuplicate(numbers)).toBe(expected)
+  })
+})
---
+++ exercises/single-element-in-sorted-array.ts
@@ -0,0 +1,26 @@
+export function singleNonDuplicate(nums: readonly number[]): number {
+  let lo = 0
+  let hi = nums.length - 1
+
+  while (lo < hi) {
+    let mi = Math.floor((hi + lo) / 2)
+
+    // 0123456
+    // 0011233
+    //    ^
+    //    hi is uneven, we always want to look at the first number of a pair
+    //   ^ shift index one to the left and continue
+    mi -= mi % 2
+
+    // 012345678
+    // 001223344
+    //     ^ right hand side is different, single number is on the left
+    if (nums[mi] !== nums[mi + 1]) hi = mi
+    // 012345678
+    // 001122344
+    //     ^ right hand side is equal, number is at least two to the right
+    else lo = mi + 2
+  }
+
+  return nums[lo]
+}
