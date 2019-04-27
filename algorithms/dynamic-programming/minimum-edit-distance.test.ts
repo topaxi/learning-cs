0c228c9d20f10af8987137d94c79ba90ff002f0c
Damian Senn <senn@puzzle.ch>
2019-04-27 15:37:42 +0200

    Add minimum edit distance

---
+++ algorithms/dynamic-programming/minimum-edit-distance.test.ts
@@ -0,0 +1,32 @@
+import {
+  minimumEditDistanceNaive,
+  minimumEditDistanceMemo
+} from './minimum-edit-distance'
+
+describe.each([minimumEditDistanceNaive, minimumEditDistanceMemo])(
+  'MinimumEditDistance',
+  minimumEditDistance => {
+    test.each<[string, string, number]>([
+      ['horse', 'ros', 3],
+      ['intention', 'execution', 5],
+      ['minimum', 'distance', 7]
+    ])(
+      `${minimumEditDistance.name}(%o, %o) is %o`,
+      (word1, word2, expectedEditDistance) => {
+        expect(minimumEditDistance(word1, word2)).toBe(expectedEditDistance)
+      }
+    )
+
+    if (minimumEditDistance === minimumEditDistanceMemo) {
+      test.each<[string, string, number]>([
+        ['Dampfschifffahrt', 'Schornsteinfeger', 13],
+        ['Dampfschifffahrtsgesellschaft', 'Schornsteinfegermeister', 24]
+      ])(
+        `${minimumEditDistance.name}(%o, %o) is %o`,
+        (word1, word2, expectedEditDistance) => {
+          expect(minimumEditDistance(word1, word2)).toBe(expectedEditDistance)
+        }
+      )
+    }
+  }
+)
---
+++ algorithms/dynamic-programming/minimum-edit-distance.ts
@@ -0,0 +1,28 @@
+import { Y, mY } from '../../utils/function/y'
+
+const minDistanceR = (
+  minDistanceR: (word1: string, word2: string, i: number, j: number) => number
+): typeof minDistanceR => (word1, word2, i, j) => {
+  if (i === 0) return j
+  if (j === 0) return i
+
+  if (word1[i - 1] === word2[j - 1])
+    return minDistanceR(word1, word2, i - 1, j - 1)
+
+  return (
+    1 +
+    Math.min(
+      minDistanceR(word1, word2, i - 1, j - 1),
+      minDistanceR(word1, word2, i - 1, j),
+      minDistanceR(word1, word2, i, j - 1)
+    )
+  )
+}
+
+export function minimumEditDistanceNaive(word1: string, word2: string) {
+  return Y(minDistanceR)(word1, word2, word1.length, word2.length)
+}
+
+export function minimumEditDistanceMemo(word1: string, word2: string) {
+  return mY(minDistanceR)(word1, word2, word1.length, word2.length)
+}
