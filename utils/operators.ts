14e8c5dd4cc7b51492d82259323c1a5fad5ed750
Damian Senn <damian.senn@gmail.com>
2019-04-30 22:26:47 +0200

    Remove unnecessary eslint directive

--- utils/operators.ts
+++ utils/operators.ts
@@ -7,7 +7,6 @@ export function add(a: number, b: number): number {
 export function concat(a: string, b: string): string
 export function concat<T>(a: LinkedList<T>, b: LinkedList<T>): LinkedList<T>
 export function concat<T>(a: ConcatArray<T>, b: ConcatArray<T>): T[]
-// eslint-disable-next-line
 export function concat(a: any, b: any): any {
   return a.concat(b)
 }
