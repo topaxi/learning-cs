1e9561a6eaaa22161a90cb661469f1924f777b62
Damian Senn <damian.senn@gmail.com>
2019-05-18 13:52:18 +0200

    iterator entries and zip tests

---
+++ utils/iterator/entries.test.ts
@@ -0,0 +1,17 @@
+import { entries } from './entries'
+
+describe('utils/iterator/entries', () => {
+  test('should iterate over values with index', () => {
+    function* values() {
+      yield 'a'
+      yield 'b'
+      yield 'c'
+    }
+
+    expect(Array.from(entries(values()))).toEqual([
+      [0, 'a'],
+      [1, 'b'],
+      [2, 'c']
+    ])
+  })
+})
---
+++ utils/iterator/zip.test.ts
@@ -0,0 +1,36 @@
+import { zip } from './zip'
+
+describe('utils/iterator/zip', () => {
+  test('should zip two iterables', () => {
+    function* numbers() {
+      yield 1
+      yield 2
+      yield 3
+    }
+    function* letters() {
+      yield 'a'
+      yield 'b'
+      yield 'c'
+    }
+
+    expect(Array.from(zip(numbers(), letters()))).toEqual([
+      [1, 'a'],
+      [2, 'b'],
+      [3, 'c']
+    ])
+  })
+
+  test('should stop on first iterable completion', () => {
+    function* numbers() {
+      yield 1
+      yield 2
+    }
+    function* letters() {
+      yield 'a'
+      yield 'b'
+      yield 'c'
+    }
+
+    expect(Array.from(zip(numbers(), letters()))).toEqual([[1, 'a'], [2, 'b']])
+  })
+})
