1aa1b2a978e9918fcdfacd167d23718142388eb1
Damian Senn <damian.senn@gmail.com>
2019-05-18 20:15:08 +0200

    Use map iterator

--- exercises/unique-emails.ts
+++ exercises/unique-emails.ts
@@ -1,5 +1,7 @@
+import { map } from '../utils/iterator/map'
+
 export function uniqueEmails(emails: readonly string[]): number {
-  return new Set(emails.map(normalize)).size
+  return new Set(map(emails, normalize)).size
 }
 
 function normalize(email: string): string {
