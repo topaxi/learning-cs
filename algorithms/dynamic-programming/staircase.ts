a8bc52499923a6f5e8d637b7b72570e3ce764bca
Damian Senn <damian.senn@gmail.com>
2019-05-19 14:58:14 +0200

    eqeqeq

--- algorithms/dynamic-programming/staircase.ts
+++ algorithms/dynamic-programming/staircase.ts
@@ -3,7 +3,7 @@ import { SingleParamStore } from '../../utils/function/memoize/single-param-stor
 
 export const staircase = (staircase: (n: number) => number) => (n: number) => {
   if (n < 1) return 0
-  if (n < 2) return 1
+  if (n === 1) return 1
   if (n === 2) return 2
 
   return staircase(n - 1) + staircase(n - 2)
