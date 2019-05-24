c6509a2e870f814d1a1087334aaa4e68ee8b0985
Damian Senn <damian.senn@gmail.com>
2019-05-24 06:54:19 +0200

    Minor simplification

--- utils/string/string-hash.ts
+++ utils/string/string-hash.ts
@@ -2,13 +2,8 @@
 export function hashCode(str: string): number {
   let hash = 0
 
-  if (str.length === 0) {
-    return hash
-  }
-
   for (let i = 0; i < str.length; i++) {
-    hash = (hash << 5) - hash + str.charCodeAt(i)
-    hash = hash & hash
+    hash = ((hash << 5) - hash + str.charCodeAt(i)) << 0
   }
 
   return hash
