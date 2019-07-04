c461e5f96ced832593ed1f9b6dfa2cc7ea24d237
Damian Senn <damian.senn@scout24.ch>
2019-07-04 17:48:31 +0200

    Explicit flip return type, eliminates an any cast

--- utils/function/flip.ts
+++ utils/function/flip.ts
@@ -1,2 +1,5 @@
-export const flip = <T, U, S>(f: (x: T) => (y: U) => S) => (y: U) => (x: T) =>
-  f(x)(y)
+export function flip<T, U, S>(
+  f: (x: T) => (y: U) => S
+): (y: U) => (x: T) => S {
+  return y => x => f(x)(y)
+}
