014e5c10031a2094376a31b23b34f6b880aa218e
Damian Senn <damian.senn@scout24.ch>
2019-07-22 07:09:47 +0200

    Disable false positive jest/valid-describe rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -64,6 +64,7 @@ module.exports = {
     {
       files: ['**/*.test.ts'],
       rules: {
+        'jest/valid-describe': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off'
