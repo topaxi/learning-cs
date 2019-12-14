50dcce92fdfd762e7eda8b2d8562c85161718b15
Damian Senn <damian.senn@scout24.ch>
2019-12-14 17:26:22 +0100

    Minor deduplication

--- algorithms/graph/bellman-ford.ts
+++ algorithms/graph/bellman-ford.ts
@@ -4,6 +4,24 @@ import { HashMap } from '../../data-structures/hash/hash-map'
 import { GraphEdge } from '../../data-structures/graph/graph-edge'
 import { GraphVertex } from '../../data-structures/graph/graph-vertex'
 
+function* relax<T>(
+  vertices: readonly GraphVertex<T>[],
+  edges: () => Iterable<GraphEdge<T>>
+) {
+  for (
+    let i = 0, didRelaxEdge = true;
+    i < lastIndex(vertices) && didRelaxEdge;
+    i++
+  ) {
+    didRelaxEdge = false
+
+    for (let edge of edges()) {
+      yield edge
+      didRelaxEdge = true
+    }
+  }
+}
+
 export function bellmanFord<T>(
   edges: readonly GraphEdge<T>[],
   vertices: readonly GraphVertex<T>[],
@@ -19,31 +37,17 @@ export function bellmanFord<T>(
     distances.get(edge.startVertex)! + edge.weight <
     distances.get(edge.endVertex)!
 
-  for (
-    let i = 0, didRelaxEdge = true;
-    i < lastIndex(vertices) && didRelaxEdge;
-    i++
-  ) {
-    didRelaxEdge = false
-    for (let edge of filter(edges, isShorterDistance)) {
-      distances.set(
-        edge.endVertex,
-        distances.get(edge.startVertex)! + edge.weight
-      )
-      didRelaxEdge = true
-    }
+  const getShorterEdges = () => filter(edges, isShorterDistance)
+
+  for (let edge of relax(vertices, getShorterEdges)) {
+    distances.set(
+      edge.endVertex,
+      distances.get(edge.startVertex)! + edge.weight
+    )
   }
 
-  for (
-    let i = 0, didRelaxEdge = true;
-    i < lastIndex(vertices) && didRelaxEdge;
-    i++
-  ) {
-    didRelaxEdge = false
-    for (let edge of filter(edges, isShorterDistance)) {
-      distances.set(edge.endVertex, -Infinity)
-      didRelaxEdge = true
-    }
+  for (let edge of relax(vertices, getShorterEdges)) {
+    distances.set(edge.endVertex, -Infinity)
   }
 
   return Array.from(distances.entries())
