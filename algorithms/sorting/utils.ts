4f766fccce906ef48e572f4c78c7318a02500f6f
Damian Senn <damian.senn@scout24.ch>
2020-01-27 16:27:36 +0100

    Expose sort implementation from sorting functions

--- algorithms/sorting/utils.ts
+++ algorithms/sorting/utils.ts
@@ -1,14 +1,19 @@
 export type ListType<T> = T extends ReadonlyArray<infer A> ? A : never
 export type Compare<T> = (a: T, b: T) => number
 
-export type SortFunction = <T>(list: Iterable<T>, compare?: Compare<T>) => T[]
+export interface SortFunction {
+  <T>(list: Iterable<T>, compare?: Compare<T>): T[]
+  sort: SortFunctionDefinition
+}
 export type SortFunctionDefinition = <T>(list: T[], compare: Compare<T>) => T[]
 
 export function defaultCompare(a: any, b: any): number {
   return a - b
 }
 
-export function define(searchFn: SortFunctionDefinition): SortFunction {
-  return (list, compare = defaultCompare) =>
-    searchFn(Array.from(list), compare)
+export function define(sort: SortFunctionDefinition): SortFunction {
+  const s = <T>(list: Iterable<T>, compare = defaultCompare) =>
+    sort(Array.from(list), compare)
+
+  return Object.defineProperty(s, 'sort', { value: sort })
 }
