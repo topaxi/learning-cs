d3d9c807c2ea3209a2493287ec177fa44bacd374
Damian Senn <damian.senn@scout24.ch>
2022-09-17 20:01:32 +0200

    feat: add new exercise

---
+++ exercises/e/0011.ts
@@ -0,0 +1,25 @@
+function getLargestProduct(grid: number[][], x: number, y: number) {
+  function get(x: number, y: number) {
+    return grid[y]?.[x] ?? 1
+  }
+
+  return Math.max(
+    get(x, y) * get(x + 1, y) * get(x + 2, y) * get(x + 3, y),
+    get(x, y) * get(x, y + 1) * get(x, y + 2) * get(x, y + 3),
+    get(x, y) * get(x + 1, y + 1) * get(x + 2, y + 2) * get(x + 3, y + 3),
+    get(x, y) * get(x - 1, y + 1) * get(x - 2, y + 2) * get(x - 3, y + 3)
+  )
+}
+
+export function solve(grid: string): number {
+  const values = grid
+    .trim()
+    .split('\n')
+    .map(r => r.split(' ').map(n => parseInt(n, 10)))
+
+  return Math.max(
+    ...values
+      .flatMap((r, y) => r.map((_value, x) => ({ x, y })))
+      .map(({ x, y }) => getLargestProduct(values, x, y))
+  )
+}
